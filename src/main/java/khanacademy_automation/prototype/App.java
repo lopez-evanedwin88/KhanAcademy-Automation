58a2047fdb9af9341a4343873f173377b123862e
tr_lnd_user <tr_lnd_user@LND-6H7ZD2S-DX.dir.svc.accenture.com>
2018-01-25 13:44:56 +0800

    Initial Commit

---
+++ src/main/java/khanacademy_automation/prototype/App.java
@@ -0,0 +1,13 @@
+package khanacademy_automation.prototype;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
